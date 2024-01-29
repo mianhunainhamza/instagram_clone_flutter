93a88a7735f70ad662587100ae1908453a104497
Areeb Rashid <121178673+Areeb-Rashid@users.noreply.github.com>
2024-01-29 17:41:56 +0500

    User name UI - Signup
    User name UI without firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:insta_clone/firebase_options.dart';
 import 'package:insta_clone/view/signup_page.dart';
 
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(),
       home: const SignUpScreen(),
--- lib/view/signup_page.dart
+++ lib/view/signup_page.dart
@@ -11,9 +11,59 @@ class SignUpScreen extends StatefulWidget {
 class _SignUpScreenState extends State<SignUpScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Padding(
+        padding: const EdgeInsets.all(28.0),
+        child: Form(
+          child: Column(
+            children: [
+              const SizedBox(height: 50),
+              const Text('Choose username',
+                style: TextStyle(
+                  fontWeight: FontWeight.w400,
+                  fontSize: 26
+                ),),
+              const SizedBox(height: 10),
+              const Text('You can always change it later',
+              style: TextStyle(
+                fontWeight: FontWeight.w200,
+                fontSize: 14
+              ),
+              ),
+              const SizedBox(height: 20),
 
+              // TExt Field
+              TextFormField(
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.symmetric(horizontal: 20),
+                  hintText: 'Username',
+                  hintStyle: const TextStyle(color: Colors.grey),
+                  floatingLabelBehavior: FloatingLabelBehavior.always,
+                  fillColor: Colors.grey.shade800,
+                  filled: true,
+                  enabledBorder:  OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  focusedBorder:  OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 14),
+              Container(
+                height: 45,
+              decoration: BoxDecoration(
+                color: Colors.blueAccent,
+                borderRadius: BorderRadius.circular(10)
+              ),
+                  child: const Center(
+                      child: Text('Next',style: TextStyle(color: Colors.white),))
+              )
+
+            ],
+          ),
+        ),
       ),
     );
   }
