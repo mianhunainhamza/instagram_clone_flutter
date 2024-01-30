3fe3809a703e6322fb04d10ffc2ba669e4275fa1
Mian Hunain <mianhunainhamza@gmail.com.com>
2024-01-31 01:39:32 +0500

    Add Story,Bottom Bar

--- lib/view/home_screen/bottom_navbar.dart
+++ lib/view/home_screen/bottom_navbar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:insta_clone/view/home_screen/home_screen.dart';
 import 'package:insta_clone/view/signup_page.dart';
 import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
@@ -14,8 +15,8 @@ class BottomNavbar extends StatelessWidget {
       context,
       controller: _controller,
       screens: _buildScreens(),
-      items: _navBarsItems(),
-      backgroundColor: Colors.black,
+      items: _navBarsItems(context),
+      backgroundColor: context.isDarkMode ? Colors.black : Colors.white,
       itemAnimationProperties: ItemAnimationProperties(
         duration: Duration(milliseconds: 200),
         curve: Curves.ease,
@@ -39,33 +40,33 @@ class BottomNavbar extends StatelessWidget {
     ];
   }
 
-  List<PersistentBottomNavBarItem> _navBarsItems() {
+  List<PersistentBottomNavBarItem> _navBarsItems(BuildContext context) {
     return [
       PersistentBottomNavBarItem(
         icon: Icon(Icons.home,size: 32,),
         title: ("Home"),
-        activeColorPrimary: CupertinoColors.white,
+        activeColorPrimary: context.isDarkMode ? Colors.white : Colors.black,
         inactiveColorPrimary: CupertinoColors.systemGrey,
       ), PersistentBottomNavBarItem(
         icon: Icon(CupertinoIcons.search,size: 32,),
         title: ("Search"),
-        activeColorPrimary: CupertinoColors.white,
+        activeColorPrimary: context.isDarkMode ? Colors.white : Colors.black,
         inactiveColorPrimary: CupertinoColors.systemGrey,
       ), PersistentBottomNavBarItem(
         icon: Icon(Icons.add_box_outlined,size: 32,),
         title: ("Story"),
-        activeColorPrimary: CupertinoColors.white,
+        activeColorPrimary: context.isDarkMode ? Colors.white : Colors.black,
         inactiveColorPrimary: CupertinoColors.systemGrey,
       ), PersistentBottomNavBarItem(
         icon: Icon(Icons.video_collection_outlined,size: 32,),
         title: ("Reels"),
-        activeColorPrimary: CupertinoColors.white,
+        activeColorPrimary: context.isDarkMode ? Colors.white : Colors.black,
         inactiveColorPrimary: CupertinoColors.systemGrey,
       ),
       PersistentBottomNavBarItem(
         icon: Icon(CupertinoIcons.person_crop_circle,size: 32,),
         title: ("Profile"),
-        activeColorPrimary: CupertinoColors.white,
+        activeColorPrimary: context.isDarkMode ? Colors.white : Colors.black,
         inactiveColorPrimary: CupertinoColors.systemGrey,
       ),
     ];
